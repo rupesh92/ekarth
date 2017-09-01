194110db4fa426a8cdbb808e733c6af6ac7ae703
Rupesh Bansal <rupesh@Rupeshs-MacBook-Pro.local>
2017-09-02 02:38:00 +0530

    Added service annotation

--- src/main/java/com/ekarth/service/LoginService.java
+++ src/main/java/com/ekarth/service/LoginService.java
@@ -3,13 +3,14 @@ package com.ekarth.service;
 import com.ekarth.dao.CustomerDAO;
 import com.ekarth.model.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 /**
  * Created by rupesh on 01/09/17.
  */
 
 
-
+@Service
 public class LoginService {
 
     @Autowired
